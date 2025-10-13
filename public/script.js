765dd140d94a5874891a4f088c0fed593cb88bab
jABurat23 <agustinquirante02@gmail.com>
2025-10-14 00:01:50 +0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyEXAMPLE",
+  authDomain: "portfolio-contact.firebaseapp.com",
+  projectId: "portfolio-contact",
+  storageBucket: "portfolio-contact.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcd1234"
+};
