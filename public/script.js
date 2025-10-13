e261cff47b734f9a4d7049102046d21cc091eff1
jABurat23 <agustinquirante02@gmail.com>
2025-10-14 00:14:19 +0800

    phase 2

--- public/script.js
+++ public/script.js
@@ -1,8 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyEXAMPLE",
-  authDomain: "portfolio-contact.firebaseapp.com",
-  projectId: "portfolio-contact",
-  storageBucket: "portfolio-contact.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcd1234"
-};
+document.getElementById("contactForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Form Submitted!");
+});
